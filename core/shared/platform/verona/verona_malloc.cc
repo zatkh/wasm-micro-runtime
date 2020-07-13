f5fb0b9c4b2270f1fadbb05d59c8892a65d6ce29
zatkh <zhtarkhani@gmail.com>
2020-07-13 12:58:53 +0100

    init iwasm-snamlloc

---
+++ core/shared/platform/verona/verona_malloc.cc
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2019 Intel Corporation.  All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
+#include "platform_api_vmcore.h"
+
+#define SNMALLOC_NAME_MANGLE(a) our_##a
+#include "/override/malloc.cc"
+
+using namespace snmalloc;
+
+void *
+os_malloc(unsigned size)
+{
+    return malloc(size);
+}
+
+void *
+os_realloc(void *ptr, unsigned size)
+{
+    return realloc(ptr, size);
+}
+
+void
+os_free(void *ptr)
+{
+    free(ptr);
+}
+
+
+
